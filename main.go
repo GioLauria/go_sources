ac13a5c1d136b8832fd81c20c40103d5469b4074
Giovanni <giovanni.lauria+coding@gmail.com>
2020-12-22 17:59:45 +0100

    #11 - Commit of 20201222 - 1608656385 - Example with variables

--- main.go
+++ main.go
@@ -9,4 +9,6 @@ func main(){
 	fmt.Println(len("Hello World"))
 	fmt.Println("Hello World"[0])
 	fmt.Println("Hello " + "World")
+	fmt.Println(true || false)
+	fmt.Println(!false)
 }
---
+++ variables.go
@@ -0,0 +1,6 @@
+package main
+import "fmt"
+func main(){
+	var x string = "This is a string"
+	fmt.Println(x)
+}
