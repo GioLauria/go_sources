037dbb05fa7c529fad512fa8d978aa4b913ef554
Giovanni <giovanni.lauria+coding@gmail.com>
2020-12-22 18:28:08 +0100

    #17 - Commit of 20201222 - 1608658087 - Example with variable scope #2

--- variable_scope.go
+++ variable_scope.go
@@ -1,8 +1,13 @@
 package main
 import "fmt"
 
-var x="This is a test out of the main"   // it works ok, global variable
+var x="Out of the main"   // it works ok, global variable
 
 func main(){
 	fmt.Println(x)
+	f()
+}
+
+func f(){
+	fmt.Println("Accessing variable",x)
 }
