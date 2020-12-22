13b6df8106b8595bf6d82f665a5a116042f79fc4
Giovanni <giovanni.lauria+coding@gmail.com>
2020-12-22 15:54:39 +0100

    #7 - Commit of 20201222 - 1608648879 - Other main.go changes

--- main.go
+++ main.go
@@ -5,4 +5,8 @@ import "fmt"
 func main(){
 	fmt.Println("Hello, World!")
 	fmt.Println("1+1=",1+2)
+	fmt.Println("1+2=",3*4)
+	fmt.Println(len("Hello World"))
+	fmt.Println("Hello World"[0])
+	fmt.Println("Hello " + "World!")
 }
